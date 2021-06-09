153e89097013d7e4a38879059c5dc20264137601
Lukas Lieb <lukas.lieb@axonivy.com>
2021-06-09 13:57:44 +0200

    Try run debug extension with lerna workspace

--- debug/src/extension.ts
+++ debug/src/extension.ts
@@ -1,16 +1,19 @@
 import * as vscode from 'vscode';
+
 import { IvyDebugAdapterServerDescriptorFactory } from './debug-adapter-factory';
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
 
-	console.log('Congratulations, your extension "debug" is now active!');
+    console.log('Congratulations, your extension "debug" is now active!');
 
     const factory = new IvyDebugAdapterServerDescriptorFactory();
 
-	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('process', factory));
-	if ('dispose' in factory) {
-		context.subscriptions.push(factory);
-	}
+    context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('process', factory));
+    if ('dispose' in factory) {
+        context.subscriptions.push(factory);
+    }
 }
 
-export function deactivate() {}
+export function deactivate() {
+    // do nothing
+}
