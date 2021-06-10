d3787e1bebd02a11f71749cef5f9580c49ab9ad8
Lukas Lieb <lukas.lieb@axonivy.com>
2021-06-10 08:36:43 +0200

    Read connection configs form launch.json

--- debug/src/debug-adapter-factory.ts
+++ debug/src/debug-adapter-factory.ts
@@ -3,8 +3,9 @@ import * as vscode from 'vscode';
 export class IvyDebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
 
     createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-
-        return new vscode.DebugAdapterServer(4711, 'localhost');
+        const host: string = session.configuration.serverHost ?? 'localhost';
+        const port: number = session.configuration.serverPort ?? 4711;
+        return new vscode.DebugAdapterServer(port, host);
     }
 
     dispose(): void {
