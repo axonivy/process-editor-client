1d625a17cb17a352fd3df32d80e4d48910c562ba
Lukas Lieb <lukas.lieb@axonivy.com>
2021-09-15 13:48:00 +0200

    Cleanup

--- editor/src/tool-palette/di.config.ts
+++ editor/src/tool-palette/di.config.ts
@@ -7,21 +7,6 @@ import { TYPES } from 'sprotty';
 import { ShowJumpOutToolFeedbackCommand } from './jump-out-tool-feedback';
 import { EnableToolPaletteAction, ToolPalette } from './tool-palette';
 
-/********************************************************************************
- * Copyright (c) 2019 EclipseSource and others.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License v. 2.0 are satisfied: GNU General Public License, version 2
- * with the GNU Classpath Exception which is available at
- * https://www.gnu.org/software/classpath/license.html.
- *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
- ********************************************************************************/
 const ivyToolPaletteModule = new ContainerModule((bind, _unbind, isBound) => {
   bind(ToolPalette).toSelf().inSingletonScope();
   bind(TYPES.IUIExtension).toService(ToolPalette);
