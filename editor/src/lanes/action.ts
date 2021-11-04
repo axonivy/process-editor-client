9fbe586d0b96343c88678bd3d6562003e6982505
Lukas Lieb <lukas.lieb@axonivy.com>
2021-11-04 14:28:38 +0100

    Fire create embedded lane action

--- editor/src/lanes/action.ts
+++ editor/src/lanes/action.ts
@@ -1,16 +1,9 @@
-import { Action, SModelElement } from '@eclipse-glsp/client';
+import { CreateNodeOperation, SModelElement } from '@eclipse-glsp/client';
 import { injectable } from 'inversify';
 
 import { LaneNode } from '../diagram/model';
 import { LaneTypes } from '../diagram/view-types';
 import { QuickAction, QuickActionLocation, SingleQuickActionProvider } from '../quick-action/quick-action';
-
-export class CreateLaneAction implements Action {
-  static readonly KIND = 'createLane';
-
-  constructor(public readonly poolId: string,
-    public readonly kind: string = CreateLaneAction.KIND) { }
-}
 
 @injectable()
 export class CreateLaneQuickActionProvider extends SingleQuickActionProvider {
@@ -28,6 +21,6 @@ class CreateLaneQuickAction implements QuickAction {
     public readonly title = 'Create Lane',
     public readonly location = QuickActionLocation.BottomLeft,
     public readonly sorting = 'A',
-    public readonly action = new CreateLaneAction(poolId)) {
+    public readonly action = new CreateNodeOperation(LaneTypes.LANE, undefined, poolId)) {
   }
 }
