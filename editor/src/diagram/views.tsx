34a947de5e0e14d8079c91c33e0af166eef99f00
Lukas Lieb <lukas.lieb@axonivy.com>
2021-11-24 16:21:22 +0100

    Fix edge padding

--- editor/src/diagram/views.tsx
+++ editor/src/diagram/views.tsx
@@ -1,5 +1,6 @@
 import {
   angleOfPoint,
+  EdgePadding,
   IView,
   Point,
   PolylineEdgeViewWithGapsOnIntersections,
@@ -40,11 +41,37 @@ export class ForeignLabelView implements IView {
 export class WorkflowEdgeView extends PolylineEdgeViewWithGapsOnIntersections {
   protected renderAdditionals(edge: SEdge, segments: Point[], context: RenderingContext): VNode[] {
     const additionals = super.renderAdditionals(edge, segments, context);
+    const edgePadding = EdgePadding.from(edge);
+    const edgePaddingNode = edgePadding ? [this.renderMouseHandle(segments, edgePadding)] : [];
+
     const p1 = segments[segments.length - 2];
     const p2 = segments[segments.length - 1];
     const arrow = <path class-sprotty-edge={true} class-arrow={true} d='M 1.5,0 L 10,-4 L 10,4 Z'
       transform={`rotate(${toDegrees(angleOfPoint({ x: p1.x - p2.x, y: p1.y - p2.y }))} ${p2.x} ${p2.y}) translate(${p2.x} ${p2.y})`} />;
-    additionals.push(arrow);
+    additionals.push(...edgePaddingNode, arrow);
     return additionals;
+  }
+
+  protected renderMouseHandle(segments: Point[], padding: number): VNode {
+    return (
+      <path
+        class-mouse-handle
+        d={this.createPathForSegments(segments)}
+        style-stroke-width={padding * 2}
+        style-stroke='transparent'
+        style-stroke-dasharray='none'
+        style-stroke-dashoffset='0'
+      />
+    );
+  }
+
+  protected createPathForSegments(segments: Point[]): string {
+    const firstPoint = segments[0];
+    let path = `M ${firstPoint.x},${firstPoint.y}`;
+    for (let i = 1; i < segments.length; i++) {
+      const p = segments[i];
+      path += ` L ${p.x},${p.y}`;
+    }
+    return path;
   }
 }
