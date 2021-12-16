3033aa6d8f5a4327491d40b997ff10321056a8d7
Lukas Lieb <lukas.lieb@axonivy.com>
2021-12-16 08:32:34 +0100

    XIVY-6710 Also raise standalone artifact version

--- raise-version.sh
+++ raise-version.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mvn --batch-mode -f integration/eclipse/webview/pom.xml versions:set versions:commit -DnewVersion=${1}
+mvn --batch-mode -f integration/standalone/pom.xml versions:set versions:commit -DnewVersion=${1}
