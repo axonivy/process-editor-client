5f7deaa059fde0be0228295606e35dd3d8a3965d
Louis Müller <louis.mueller@axonivy.com>
2022-02-21 09:52:31 +0100

    Update colorize operation parameter

--- editor/src/tool-bar/operation.ts
+++ editor/src/tool-bar/operation.ts
@@ -9,5 +9,10 @@ export class AutoAlignOperation implements Operation {
 export class ColorizeOperation implements Operation {
   static readonly KIND = 'colorize';
 
-  constructor(public readonly elementIds: string[], public readonly color: string, public readonly kind: string = ColorizeOperation.KIND) {}
+  constructor(
+    public readonly elementIds: string[],
+    public readonly color: string,
+    public readonly colorName: string,
+    public readonly kind: string = ColorizeOperation.KIND
+  ) {}
 }
--- editor/src/tool-bar/tool-bar.ts
+++ editor/src/tool-bar/tool-bar.ts
@@ -240,7 +240,7 @@ export class ToolBar extends AbstractUIExtension implements IActionHandler, Edit
   onClickElementPickerToolButton = (button: HTMLElement, item: PaletteItem): void => {
     if (!this.editorContext.isReadonly) {
       if (item.actions.length === 1 && item.id.startsWith('color-palette-item') && item.icon) {
-        item.actions[0] = new ColorizeOperation([...this.selectionService.getSelectedElementIDs()], item.icon);
+        item.actions[0] = new ColorizeOperation([...this.selectionService.getSelectedElementIDs()], item.icon, item.label);
       }
       this.dispatchAction(item.actions);
       this.changeActiveButton(button);
