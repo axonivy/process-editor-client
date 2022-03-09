b056777c6a2513250d9997014c9efffaf8f92511
Louis Müller <louis.mueller@axonivy.com>
2022-03-09 16:12:32 +0100

    XIVY-6713 Update quick action position.

--- editor/src/key-listener/change-bounds.ts
+++ editor/src/key-listener/change-bounds.ts
@@ -1,17 +1,24 @@
 import { SelectionService } from '@eclipse-glsp/client/lib/features/select/selection-service';
-import { KeyListener, Action, SModelElement, BoundsAware } from 'sprotty';
+import { KeyListener, Action, SModelElement, BoundsAware, SChildElement } from 'sprotty';
 import { matchesKeystroke } from 'sprotty/lib/utils/keyboard';
 import { inject } from 'inversify';
-import { GLSP_TYPES, isNonRoutableSelectedMovableBoundsAware, toElementAndBounds } from '@eclipse-glsp/client';
+import {
+  GLSP_TYPES,
+  isNonRoutableSelectedMovableBoundsAware,
+  SetUIExtensionVisibilityAction,
+  toElementAndBounds
+} from '@eclipse-glsp/client';
 import { ChangeBoundsOperation, ElementAndBounds } from '@eclipse-glsp/protocol';
 import { IvyGridSnapper } from '../diagram/snap';
+import { QuickActionUI } from '../quick-action/quick-action-ui';
 
-export class ChangeBoundsKeyListener extends KeyListener {
+export class MoveElementKeyListener extends KeyListener {
   @inject(GLSP_TYPES.SelectionService) protected selectionService: SelectionService;
   snapper = new IvyGridSnapper();
 
   keyDown(element: SModelElement, event: KeyboardEvent): Action[] {
-    const selectedElements = this.selectionService.getSelectedElements().filter(isNonRoutableSelectedMovableBoundsAware);
+    let selectedElements = this.selectionService.getSelectedElements().filter(isNonRoutableSelectedMovableBoundsAware);
+    selectedElements = selectedElements.filter(e => !this.isChildOfSelected(selectedElements, e));
     let deltaX = 0;
     let deltaY = 0;
     if (selectedElements.length === 0) {
@@ -27,18 +34,25 @@ export class ChangeBoundsKeyListener extends KeyListener {
     } else {
       return [];
     }
-    const newBounds = selectedElements.map(e => this.getElmentAndUpdatedBound(e, deltaX, deltaY));
-    return [new ChangeBoundsOperation(newBounds)];
+    const newBounds = selectedElements.map(e => this.getElementAndUpdatedBound(e, deltaX, deltaY));
+    return [
+      new ChangeBoundsOperation(newBounds),
+      new SetUIExtensionVisibilityAction(QuickActionUI.ID, true, [...selectedElements.map(e => e.id)])
+    ];
   }
 
-  protected getElmentAndUpdatedBound(element: SModelElement & BoundsAware, deltaX: number, deltaY: number): ElementAndBounds {
-    const newBound = toElementAndBounds(element);
+  protected isChildOfSelected(selectedElements: SModelElement[], element: SModelElement): boolean {
+    return element instanceof SChildElement && selectedElements.includes(element.parent);
+  }
+
+  protected getElementAndUpdatedBound(element: SModelElement & BoundsAware, deltaX: number, deltaY: number): ElementAndBounds {
+    const bound = toElementAndBounds(element);
     let newPosition = {
       x: element.bounds.x + deltaX,
       y: element.bounds.y + deltaY
     };
     newPosition = this.snapper.snap(newPosition, element);
-    newBound.newPosition = newPosition;
-    return newBound;
+    bound.newPosition = newPosition;
+    return bound;
   }
 }
