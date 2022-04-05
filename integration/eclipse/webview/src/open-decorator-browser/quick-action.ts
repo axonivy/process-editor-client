4fddaeb3a77a84053d65f0cae53ea81a0ca058e5
Lukas Lieb <lukas.lieb@axonivy.com>
2022-04-05 14:13:46 +0200

    XIVY-8047 Remove reset flag from action - Let core decide what should be done

--- integration/eclipse/webview/src/open-decorator-browser/quick-action.ts
+++ integration/eclipse/webview/src/open-decorator-browser/quick-action.ts
@@ -1,12 +1,5 @@
 import { SModelElement } from '@eclipse-glsp/client';
-import {
-  QuickAction,
-  QuickActionLocation,
-  SingleQuickActionProvider,
-  isWithCustomIcon,
-  WithCustomIcon,
-  ActivityTypes
-} from '@ivyteam/process-editor';
+import { QuickAction, QuickActionLocation, SingleQuickActionProvider, isWithCustomIcon, ActivityTypes } from '@ivyteam/process-editor';
 import { injectable } from 'inversify';
 import { OpenDecoratorBrowserAction } from './action';
 
@@ -14,24 +7,19 @@ import { OpenDecoratorBrowserAction } from './action';
 export class CustomIconQuickActionProvider extends SingleQuickActionProvider {
   singleQuickAction(element: SModelElement): QuickAction | undefined {
     if (isWithCustomIcon(element) && element.type !== ActivityTypes.COMMENT) {
-      return new CustomIconQuickAction(element.id, this.isCustomIconSet(element));
+      return new CustomIconQuickAction(element.id);
     }
     return undefined;
-  }
-
-  private isCustomIconSet(element: WithCustomIcon): boolean {
-    return element.icon !== element.customIcon;
   }
 }
 
 class CustomIconQuickAction implements QuickAction {
   constructor(
     public readonly elementId: string,
-    public readonly customIconSet: boolean,
     public readonly icon = 'fa-image',
     public readonly title = 'Custom Icon',
     public readonly location = QuickActionLocation.TopLeft,
     public readonly sorting = 'C',
-    public readonly action = new OpenDecoratorBrowserAction(elementId, customIconSet)
+    public readonly action = new OpenDecoratorBrowserAction(elementId)
   ) {}
 }
