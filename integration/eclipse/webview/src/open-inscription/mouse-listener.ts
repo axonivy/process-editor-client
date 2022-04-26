effbb668c876a1158987f2a2be0fb38c4c47fc67
Lukas Lieb <lukas.lieb@axonivy.com>
2022-04-26 13:54:48 +0200

    XIVY-8280 Open inscription on label double click

--- integration/eclipse/webview/src/open-inscription/mouse-listener.ts
+++ integration/eclipse/webview/src/open-inscription/mouse-listener.ts
@@ -1,11 +1,12 @@
-import { isOpenable, MouseListener, SModelElement } from '@eclipse-glsp/client';
+import { findParentByFeature, isOpenable, MouseListener, SModelElement } from '@eclipse-glsp/client';
 import { Action } from '@eclipse-glsp/protocol';
 import { OpenInscriptionAction } from './open-inscription-handler';
 
 export class OpenInscriptionMouseListener extends MouseListener {
   doubleClick(target: SModelElement, event: MouseEvent): (Action | Promise<Action>)[] {
-    if (isOpenable(target)) {
-      return [new OpenInscriptionAction(target.id)];
+    const element = findParentByFeature(target, isOpenable);
+    if (element) {
+      return [new OpenInscriptionAction(element.id)];
     }
     return [];
   }
