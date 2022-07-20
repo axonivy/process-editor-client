d6ae5f5b8146754474ddb838adeeb30f16a3722e
Lukas Lieb <lukas.lieb@axonivy.com>
2022-07-20 16:14:21 +0200

    Move grid with scroll/zoom

--- editor/src/diagram/grid/action-handler.ts
+++ editor/src/diagram/grid/action-handler.ts
@@ -1,5 +1,15 @@
-import { Action, hasBooleanProp, IActionDispatcher, IActionHandler, IFeedbackActionDispatcher, TYPES } from '@eclipse-glsp/client';
+import {
+  Action,
+  hasBooleanProp,
+  IActionDispatcher,
+  IActionHandler,
+  IFeedbackActionDispatcher,
+  SetViewportAction,
+  TYPES,
+  ViewerOptions
+} from '@eclipse-glsp/client';
 import { inject, injectable } from 'inversify';
+import { IvyGridSnapper } from '../snap';
 import { GridFeedbackAction } from './feedback-action';
 
 export interface ShowGridAction extends Action {
@@ -28,6 +38,7 @@ export class ShowGridActionHandler implements IActionHandler {
 
   @inject(TYPES.IFeedbackActionDispatcher) protected feedbackDispatcher: IFeedbackActionDispatcher;
   @inject(TYPES.IActionDispatcher) protected actionDispatcher: IActionDispatcher;
+  @inject(TYPES.ViewerOptions) protected options: ViewerOptions;
 
   handle(action: Action): Action | void {
     if (ShowGridAction.is(action)) {
@@ -37,6 +48,20 @@ export class ShowGridActionHandler implements IActionHandler {
       } else {
         this.feedbackDispatcher.deregisterFeedback(this, [GridFeedbackAction.create({ show: action.show })]);
       }
+    }
+    if (SetViewportAction.is(action)) {
+      this.moveGrid(action);
+    }
+  }
+
+  moveGrid(action: SetViewportAction): void {
+    const baseDiv = document.querySelector(`#${this.options.baseDiv} .sprotty-graph`) as HTMLElement;
+    if (baseDiv) {
+      const newPosX = (-action.newViewport.scroll.x + IvyGridSnapper.GRID_X) * action.newViewport.zoom;
+      const newPosY = (-action.newViewport.scroll.y + IvyGridSnapper.GRID_Y) * action.newViewport.zoom;
+      baseDiv.style.backgroundPosition = `${newPosX}px ${newPosY}px`;
+      const newSize = IvyGridSnapper.GRID_X * 2 * action.newViewport.zoom;
+      baseDiv.style.backgroundSize = `${newSize}px ${newSize}px`;
     }
   }
 
