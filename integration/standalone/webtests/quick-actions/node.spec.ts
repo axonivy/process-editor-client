0248933ad96b869721c066f18da3169b5308d01d
Lukas Lieb <lukas.lieb@axonivy.com>
2022-09-30 11:34:25 +0200

    XIVY-9548 Support wrap single element to sub

--- integration/standalone/webtests/quick-actions/node.spec.ts
+++ integration/standalone/webtests/quick-actions/node.spec.ts
@@ -14,13 +14,13 @@ test.describe('quick actions - nodes', () => {
 
     await assertQuickActionsCount(page, 0);
     await start.click();
-    await assertQuickActionsCount(page, 7);
+    await assertQuickActionsCount(page, 8);
     await end.click();
-    await assertQuickActionsCount(page, 4);
+    await assertQuickActionsCount(page, 5);
 
     await removeElement(page, endSelector);
     await start.click();
-    await assertQuickActionsCount(page, 8);
+    await assertQuickActionsCount(page, 9);
   });
 
   test('label edit', async ({ page }) => {
