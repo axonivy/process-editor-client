fb97930409571cd4040bd8dba9c96aebcbc71953
Lukas Lieb <lukas.lieb@axonivy.com>
2022-10-17 08:37:27 +0200

    Add own ivy test project for webtests

--- .ivy/raise-build-plugin-version.sh
+++ .ivy/raise-build-plugin-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn --batch-mode versions:set-property versions:commit -f integration/standalone/pom.webtest.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
+mvn --batch-mode versions:set-property versions:commit -f integration/standalone/glsp-test-project/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
--- integration/standalone/webtests/url-param/url-param.spec.ts
+++ integration/standalone/webtests/url-param/url-param.spec.ts
@@ -48,7 +48,7 @@ test.describe('url parameters', () => {
   });
 
   test('zoom and highlight', async ({ page }) => {
-    await page.goto(procurementRequestParallelUrl() + '&zoom=321&highlight=15254DC87A1B183B-f0');
+    await page.goto(urlParamProcess() + '&zoom=321&highlight=183E49FD86C52941-f0');
     const start = page.locator(startSelector + '.executed');
     const viewport = page.locator('.ivy-viewport-bar');
     await expect(start).toBeVisible();
@@ -57,7 +57,7 @@ test.describe('url parameters', () => {
   });
 
   test('zoom and selectElementIds', async ({ page }) => {
-    await page.goto(procurementRequestParallelUrl() + '&zoom=123&selectElementIds=15254DC87A1B183B-f0');
+    await page.goto(urlParamProcess() + '&zoom=123&selectElementIds=183E49FD86C52941-f0');
     const start = page.locator(startSelector + '.selected');
     const viewport = page.locator('.ivy-viewport-bar');
     await expect(start).toBeVisible();
@@ -67,29 +67,29 @@ test.describe('url parameters', () => {
 
   test('theme light', async ({ page }) => {
     await page.emulateMedia({ colorScheme: 'dark' });
-    await page.goto(procurementRequestParallelUrl());
+    await page.goto(urlParamProcess());
     const html = page.locator('html');
     await expect(html).toHaveAttribute('data-theme', 'dark');
 
-    await page.goto(procurementRequestParallelUrl() + '&theme=light');
+    await page.goto(urlParamProcess() + '&theme=light');
     await expect(html).toHaveAttribute('data-theme', 'light');
   });
 
   test('theme dark', async ({ page }) => {
     await page.emulateMedia({ colorScheme: 'light' });
-    await page.goto(procurementRequestParallelUrl());
+    await page.goto(urlParamProcess());
     const html = page.locator('html');
     await expect(html).toHaveAttribute('data-theme', 'light');
 
-    await page.goto(procurementRequestParallelUrl() + '&theme=dark');
+    await page.goto(urlParamProcess() + '&theme=dark');
     await expect(html).toHaveAttribute('data-theme', 'dark');
   });
 
-  function procurementRequestParallelUrl(): string {
-    return workflowDemosUrl('/processes/Humantask/ProcurementRequestParallel.p.json');
+  function urlParamProcess(): string {
+    return testProjectUrl('/processes/urlparam.p.json');
   }
 
-  function workflowDemosUrl(file: string): string {
-    return processEditorUrl('workflow-demos', file);
+  function testProjectUrl(file: string): string {
+    return processEditorUrl('glsp-test-project', file);
   }
 });
