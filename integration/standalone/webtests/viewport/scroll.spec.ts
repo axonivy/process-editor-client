ae4ad05d993f7405255149cd943c27bceda47cdb
Lukas Lieb <lukas.lieb@axonivy.com>
2022-11-02 08:29:31 +0100

    XIVY-9913 White tool-bar in light mode

--- integration/standalone/webtests/viewport/scroll.spec.ts
+++ integration/standalone/webtests/viewport/scroll.spec.ts
@@ -10,21 +10,21 @@ test.describe('viewport bar', () => {
   });
 
   test('scroll vertical', async ({ page }) => {
-    await scrollAndAssert(page, { x: 0, y: 100 }, 'scale(1) translate(0,-52)', { x: 0, y: -100 });
-    await scrollAndAssert(page, { x: 0, y: -200 }, 'scale(1) translate(0,148)', { x: 0, y: 100 });
+    await scrollAndAssert(page, { x: 0, y: 100 }, 'scale(1) translate(0,-48)', { x: 0, y: -100 });
+    await scrollAndAssert(page, { x: 0, y: -200 }, 'scale(1) translate(0,152)', { x: 0, y: 100 });
     await scrollAndAssert(page, { x: 0, y: 100 }, ORIGIN_VIEWPORT, { x: 0, y: 0 });
   });
 
   test('scroll horizontal', async ({ page }) => {
-    await scrollAndAssert(page, { x: 100, y: 0 }, 'scale(1) translate(-100,48)', { x: -100, y: 0 });
-    await scrollAndAssert(page, { x: -200, y: 0 }, 'scale(1) translate(100,48)', { x: 100, y: 0 });
+    await scrollAndAssert(page, { x: 100, y: 0 }, 'scale(1) translate(-100,52)', { x: -100, y: 0 });
+    await scrollAndAssert(page, { x: -200, y: 0 }, 'scale(1) translate(100,52)', { x: 100, y: 0 });
     await scrollAndAssert(page, { x: 100, y: 0 }, ORIGIN_VIEWPORT, { x: 0, y: 0 });
   });
 
   test('scroll horizontal (shift)', async ({ page }) => {
     await page.keyboard.down('Shift');
-    await scrollAndAssert(page, { x: 0, y: 100 }, 'scale(1) translate(-100,48)', { x: -100, y: 0 });
-    await scrollAndAssert(page, { x: 0, y: -200 }, 'scale(1) translate(100,48)', { x: 100, y: 0 });
+    await scrollAndAssert(page, { x: 0, y: 100 }, 'scale(1) translate(-100,52)', { x: -100, y: 0 });
+    await scrollAndAssert(page, { x: 0, y: -200 }, 'scale(1) translate(100,52)', { x: 100, y: 0 });
     await scrollAndAssert(page, { x: 0, y: 100 }, ORIGIN_VIEWPORT, { x: 0, y: 0 });
     await page.keyboard.up('Shift');
   });
