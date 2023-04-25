1dfb99a97c93515dfb6a2b656070a94810bea223
Christian Strebel <christian.strebel@axonivy.com>
2023-04-25 17:27:22 +0200

    XIVY-10876 Raise versions in package.json

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -3,3 +3,7 @@
 mvn --batch-mode -f integration/eclipse/webview/pom.xml versions:set versions:commit -DnewVersion=${1}
 mvn --batch-mode -f integration/standalone/pom.xml versions:set versions:commit -DnewVersion=${1}
 mvn --batch-mode -f integration/standalone/glsp-test-project/pom.xml versions:set versions:commit -DnewVersion=${1}
+
+npm install -g yarn
+yarn install --ignore-scripts
+yarn lerna version ${1/SNAPSHOT/next} --no-git-tag-version --no-push --ignore-scripts --exact --yes
