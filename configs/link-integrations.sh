aa7d7bf39028d9efc78a7d12064561cee266f2d5
Louis <louis.mueller@axonivy.com>
2023-05-08 09:09:56 +0200

    XIVY-11010 remove vscode integration

--- configs/link-integrations.sh
+++ configs/link-integrations.sh
@@ -8,7 +8,6 @@ yarn link --cwd editor
 yarn unlink --cwd node_modules/sprotty
 yarn link --cwd node_modules/sprotty
 
-
 yarn unlink --cwd node_modules/@eclipse-glsp/client
 yarn link --cwd node_modules/@eclipse-glsp/client
 
@@ -18,17 +17,12 @@ yarn link --cwd node_modules/@eclipse-glsp/protocol
 yarn unlink --cwd node_modules/vscode-jsonrpc
 yarn link --cwd node_modules/vscode-jsonrpc
 
-yarn link @axonivy/process-editor --cwd integration/vscode
 yarn link @axonivy/process-editor --cwd integration/eclipse
 
-yarn link sprotty --cwd integration/vscode
 yarn link sprotty --cwd integration/eclipse
 
-yarn link @eclipse-glsp/client --cwd integration/vscode
 yarn link @eclipse-glsp/client --cwd integration/eclipse
 
-yarn link @eclipse-glsp/protocol --cwd integration/vscode
 yarn link @eclipse-glsp/protocol --cwd integration/eclipse
 
-yarn link vscode-jsonrpc --cwd integration/vscode
 yarn link vscode-jsonrpc --cwd integration/eclipse
--- integration/vscode/README.md
+++
@@ -1,7 +0,0 @@
-# Axon Ivy Process Editor - VS Code integration
-
-This package contains the VS Code integration for the Axon Ivy (GLSP-based) process editor.
-
-## More information
-
-For more information about how to build and run this integration checkout the repository [README.md](../../README.md)
