a6eca4c5c0c131a505a63e63af14b9a6b8e662c0
Lukas Lieb <lukas.lieb@axonivy.com>
2023-11-08 16:22:08 +0100

    XIVY-12602 Add viewer integration

---
+++ integration/viewer/src/index.ts
@@ -0,0 +1,4 @@
+// inversify: The reflect-metadata polyfill should be imported only once in your entire application because the Reflect object is meant to be a global singleton
+// the reflect-metadata import must be first!
+import 'reflect-metadata';
+import './app';
