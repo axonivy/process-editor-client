cadf251869c9a132ea33c1e647254d1771a5de95
Lukas Lieb <lukas.lieb@axonivy.com>
2023-11-09 12:03:45 +0100

    XIVY-12602 Save current resize width

---
+++ packages/inscription/src/inscription/inscription-width-storage.ts
@@ -0,0 +1,34 @@
+export const inscriptionWidthStorage = () => {
+  const inscriptionWidthKey = 'inscription-view-width';
+  const fixWidth = (newWidth: number) => {
+    if (newWidth > window.innerWidth - 250) {
+      return window.innerWidth - 250;
+    }
+    if (newWidth < 200) {
+      return 200;
+    }
+    return newWidth;
+  };
+
+  const setWidth = (width: number) => {
+    try {
+      localStorage.setItem(inscriptionWidthKey, `${width}`);
+    } catch (e) {
+      console.warn("couldn't save width to localStorage", e);
+    }
+  };
+
+  const getWidth = () => {
+    let width = window.innerWidth / 3;
+    if (width) {
+      try {
+        width = Number(localStorage.getItem(inscriptionWidthKey));
+      } catch (e) {
+        console.warn("couldn't load width from localStorage", e);
+      }
+    }
+    return fixWidth(width);
+  };
+
+  return { setWidth, getWidth, fixWidth };
+};
