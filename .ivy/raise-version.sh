75c95e98f185f00879de2f456d4e52569aca844b
Alexander Suter <alexander.suter@axonivy.com>
2023-12-27 07:38:44 +0100

    Fix raise-version.sh

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -3,7 +3,7 @@ set -e
 
 mvn --batch-mode -f integration/eclipse/pom.xml versions:set versions:commit -DnewVersion=${1}
 mvn --batch-mode -f integration/viewer/pom.xml versions:set versions:commit -DnewVersion=${1}
-mvn --batch-mode -f integration/standalone/glsp-test-project/pom.xml versions:set versions:commit -DnewVersion=${1}
+mvn --batch-mode -f playwright/glsp-test-project/pom.xml versions:set versions:commit -DnewVersion=${1}
 
 yarn install --ignore-scripts
 yarn lerna version ${1/SNAPSHOT/next} --no-git-tag-version --no-push --ignore-scripts --exact --yes
