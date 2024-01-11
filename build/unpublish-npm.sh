2cf17ee247aded61bea1e5de9a0c3e141abf77cd
Christian Strebel <christian.strebel@axonivy.com>
2024-01-11 22:05:17 +0100

    Use npmjs-registry.ivyteam.ch

--- build/unpublish-npm.sh
+++ build/unpublish-npm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REGISTRY="https://npmjs-registry.ivyteam.io/"
+REGISTRY="https://npmjs-registry.ivyteam.ch/"
 
 npm unpublish "@axonivy/process-editor@${1}" --registry $REGISTRY
 npm unpublish "@axonivy/process-editor-inscription@${1}" --registry $REGISTRY
