938761bcd8d391dd547f4f65f9245faaa19da591
Louis <louis.mueller@axonivy.com>
2024-03-27 12:27:25 +0100

    XIVY-12139 viewer test if read only and jump work

---
+++ playwright/tests/viewer/navigation/jump-to-external-target.spec.ts
@@ -0,0 +1,8 @@
+import { test } from '@playwright/test';
+import { jumpToExternalTargetAndAssert } from '../../standalone/navigation/jump-to-external-target.spec';
+
+test.describe('Jump to external target', () => {
+  test('hd process with viewer', async ({ page }) => {
+    await jumpToExternalTargetAndAssert(page, '183E4A356E771204-f5', '183E4A455276AFC5', '183E4A455276AFC5-f0');
+  });
+});
