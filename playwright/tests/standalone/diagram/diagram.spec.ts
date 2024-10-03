e93944c6e80ce297045a63b5390abd0488fd2d4d
Lukas Lieb <lukas.lieb@axonivy.com>
2024-10-03 14:25:19 +0200

    XIVY-15151 Add test for sub sub markers
    - Hide process markers

--- playwright/tests/standalone/diagram/diagram.spec.ts
+++ playwright/tests/standalone/diagram/diagram.spec.ts
@@ -1,10 +1,29 @@
 import { test } from '@playwright/test';
 import { ProcessEditor } from '../../page-objects/process-editor';
 
-test.describe('diagram', () => {
-  test('open process editor', async ({ page }) => {
-    const processEditor = await ProcessEditor.openProcess(page);
-    const toolPalette = processEditor.toolbar();
-    await toolPalette.visible();
-  });
+test('diagram', async ({ page }) => {
+  const processEditor = await ProcessEditor.openProcess(page);
+  const toolPalette = processEditor.toolbar();
+  await toolPalette.visible();
+});
+
+test('problem marker', async ({ page }) => {
+  const editor = await ProcessEditor.openProcess(page);
+  const hd = await editor.createActivity('User Dialog', { x: 100, y: 100 });
+  await hd.expectHasWarning();
+
+  await hd.quickActionBar().trigger('Wrap', 'startsWith');
+  const sub = editor.element('embeddedProcessElement');
+  await sub.expectHasWarning();
+
+  await sub.quickActionBar().trigger('Jump', 'startsWith');
+  const subHd = editor.element('dialogCall');
+  await subHd.expectHasWarning();
+
+  await subHd.quickActionBar().trigger('Wrap', 'startsWith');
+  const subSub = editor.element('embeddedProcessElement');
+  await subSub.expectHasWarning();
+
+  await editor.jumpOut().click();
+  await sub.expectHasWarning();
 });
