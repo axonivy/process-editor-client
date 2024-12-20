b09e4382206a4258cc02aa55da26c10dc70093ca
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-12-20 11:58:41 +0100

    XIVY-13943 extended sub-process connector example

--- playwright/tests/standalone/tool-bar/extensions.spec.ts
+++ playwright/tests/standalone/tool-bar/extensions.spec.ts
@@ -1,20 +1,20 @@
 import { expect, test } from '@playwright/test';
 import { ProcessEditor } from '../../page-objects/editor/process-editor';
 
-test('extnsions with entries', async ({ page }) => {
+test('extensions with entries', async ({ page }) => {
   const editor = await ProcessEditor.openEmptyProcess(page);
   await editor.toolbar().openElementPalette('extensions');
   const menu = editor.toolbar().menu();
 
   await menu.expectMenuGroups(['Rest Clients', 'Callable Sub Processes']);
-  await menu.expectMenuItemCount(4);
+  await menu.expectMenuItemCount(5);
   const items = menu.locator().locator('.menu-item');
   await expect(items.nth(0)).toContainText('genderizehttp://localhost/');
   await expect(items.nth(0).locator('img')).toHaveAttribute('src', /javax.faces.resource\/icons\/genderize.png/);
   await expect(items.nth(1)).toContainText('testhttp://localhost/');
-  await expect(items.nth(2)).toContainText('subproc');
-  await expect(items.nth(3)).toContainText('subWithIcon');
-  await expect(items.nth(3).locator('img')).toHaveAttribute('src', /javax.faces.resource\/icons\/Developer.jpg/);
+  await expect(items.nth(3)).toContainText('subproc');
+  await expect(items.nth(4)).toContainText('subWithIcon');
+  await expect(items.nth(4).locator('img')).toHaveAttribute('src', /javax.faces.resource\/icons\/Developer.jpg/);
 
   await menu.search('gen');
   await menu.expectMenuItemCount(1);
