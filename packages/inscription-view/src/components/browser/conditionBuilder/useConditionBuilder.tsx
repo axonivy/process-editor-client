5a235a6feedd5eebc87e66f1b7ed17ba2e8b567a
Lukas Lieb <lukas.lieb@axonivy.com>
2025-01-24 11:51:05 +0100

    Adapt condition builder changes

--- packages/inscription-view/src/components/browser/conditionBuilder/useConditionBuilder.tsx
+++ packages/inscription-view/src/components/browser/conditionBuilder/useConditionBuilder.tsx
@@ -1,9 +1,9 @@
 import { IvyIcons } from '@axonivy/ui-icons';
 import type { UseBrowserImplReturnValue } from '../useBrowser';
 import type { BrowserValue } from '../Browser';
-import { useEffect, useState } from 'react';
-import { generateConditionString, logicalOperatorOptions, typeOptions } from './conditionBuilderData';
-import { ConditionBuilder, ConditionBuilderProvider } from '@axonivy/ui-components';
+import { useState } from 'react';
+import { generateConditionString, logicOperators, operators } from './conditionBuilderData';
+import { ConditionBuilder } from '@axonivy/ui-components';
 import InputWithBrowser from '../../widgets/input/InputWithBrowser';
 
 export const CONDITION_BUILDER_ID = 'condition' as const;
@@ -25,20 +25,15 @@ type ConditionBrowserProps = {
 };
 
 const ConditionBrowser = ({ value, onChange }: ConditionBrowserProps) => {
-  const [condition, setCondition] = useState('');
-  useEffect(() => {
-    onChange({ cursorValue: condition });
-  }, [condition, onChange]);
   return (
     <>
-      <ConditionBuilderProvider
+      <ConditionBuilder
+        onChange={change => onChange({ cursorValue: change })}
         generateConditionString={generateConditionString}
-        logicalOperatorOptions={logicalOperatorOptions}
-        typeOptions={typeOptions}
+        logicOperators={logicOperators}
+        operators={operators}
         argumentInput={(value, onChange) => <InputWithBrowser value={value} onChange={onChange} browsers={['attr']} style={{ flex: 1 }} />}
-      >
-        <ConditionBuilder onChange={setCondition} />
-      </ConditionBuilderProvider>
+      />
       <pre className='browser-helptext'>
         <b>Generated Condition</b>
         <code>{value}</code>
