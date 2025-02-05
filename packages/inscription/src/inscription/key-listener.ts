d5ab080766aca77b71c054f7e63b9012ca720717
Louis <louis.mueller@axonivy.com>
2025-02-05 16:35:34 +0100

    wip

---
+++ packages/inscription/src/inscription/key-listener.ts
@@ -0,0 +1,32 @@
+import { FocusDomAction, GlobalKeyListenerTool, matchesKeystroke, SetAccessibleKeyShortcutAction } from '@eclipse-glsp/client';
+import { ToggleInscriptionAction } from './action';
+import { injectable } from 'inversify';
+
+@injectable()
+export class IvyInscriptionGlobalKeyListenerTool extends GlobalKeyListenerTool {
+  registerShortcutKey(): void {
+    this.actionDispatcher.onceModelInitialized().then(() => {
+      this.actionDispatcher.dispatchAll([
+        SetAccessibleKeyShortcutAction.create({
+          token: 'Inscription',
+          keys: [{ shortcuts: ['3'], description: 'Focus on inscription', group: 'Inscription', position: 0 }]
+        })
+      ]);
+    });
+  }
+
+  get id(): string {
+    return 'glsp.global-inscription-key-listener';
+  }
+
+  protected handleKeyEvent(event: KeyboardEvent) {
+    if (this.matchesSetFocusOnInscription(event)) {
+      return [ToggleInscriptionAction.create({}), FocusDomAction.create('#inscription-ui button')];
+    }
+    return [];
+  }
+
+  protected matchesSetFocusOnInscription(event: KeyboardEvent): boolean {
+    return matchesKeystroke(event, 'Digit3') || matchesKeystroke(event, 'Numpad3');
+  }
+}
